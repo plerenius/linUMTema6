a86bbc7dcea1a4ef7f43c4f62f7565ca65188fcd
Petter <petter@lerenius.se>
2014-02-17 21:50:04 +0100

    Added mainfile and structure

---
+++ src/electrotest.c
@@ -0,0 +1,34 @@
+/*
+
+Main program for lab6
+
+*/
+#include <stdio.h>
+#include "resistance.h"
+
+
+int main()
+{
+  int count, i;
+  float totRes;
+  float *array;
+  char conn;
+  printf("Ange spänningskälla i V:");
+  printf("Ange koppling[S | P]:");
+  if (scanf("%c", &conn)!=1) { printf ("error - it is not an char\n"); return -1; }
+  printf("Antal komponenter:");
+  if (scanf("%i", &count)!=1) { printf ("error - it is not an integer\n"); return -1; }
+  array = (float*)  malloc(sizeof(float) * count);
+  for (i=0; i < count; i++)
+  {
+    printf("Komponent %d i ohm:",i + 1);
+    if (scanf("%f", &array[i])!=1) { printf ("error - it is not an float\n"); return -1; }
+  }
+
+  // Calculate values
+  printf("Ersättningsresistans: %.2f ohm\n",calc_resistance(count,conn,array));
+  printf("Effekt: %f W\n",1.78);
+  printf("Ersättningsresistanser i E12-serien kopplade i serie:");
+  free(array);
+  return 0;
+}
